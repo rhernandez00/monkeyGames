25aa41f63333c3b21c0da2ec379c960d7a6ac3a8
Raúl Hernández <raul@lafuentelab.org>
2017-05-20 12:34:34 -0500

    Added button control, play, pause, unpause, rewind

--- simpleAudio.py
+++ simpleAudio.py
@@ -1,6 +1,30 @@
 import pygame
+import RPi.GPIO as GPIO
+
+input1 = 16
+input2 = 20
+input3 = 21
+input4 = 26
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(input1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(input2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(input3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(input4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+GPIO.add_event_detect(input1, GPIO.RISING)
+GPIO.add_event_detect(input2, GPIO.RISING)
+GPIO.add_event_detect(input3, GPIO.RISING)
+GPIO.add_event_detect(input4, GPIO.RISING)
+GPIO.add_event_callback(input1, pygame.mixer.music.play)
+GPIO.add_event_callback(input2, pygame.mixer.music.pause)
+GPIO.add_event_callback(input3, pygame.mixer.music.unpause)
+GPIO.add_event_callback(input4, pygame.mixer.music.rewind)
+
+
 pygame.mixer.init()
-pygame.mixer.music.load("myFile.wav")
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy() == True:
-    continue
+pygame.mixer.music.load("test.wav")
+
+
+
+
